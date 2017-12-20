b5d79e3aa12aeadf6da09f90b04aecbdfa6a2e69
houny <houny@163.com>
2017-12-20 16:11:58 +0800

    拆分后的聚合工程,将web提出,分布式soa架构

--- e3-manager/e3-manager-interface/src/main/java/cn/e3mall/service/ItemService.java
+++ e3-manager/e3-manager-interface/src/main/java/cn/e3mall/service/ItemService.java
@@ -1,10 +1,13 @@
 package cn.e3mall.service;
 
+import cn.e3mall.common.pojo.DataGridResult;
 import cn.e3mall.pojo.TbItem;
 
 public interface ItemService {
 
 	TbItem getItemById(long id);
+
+	DataGridResult findAllItem(int page, int rows);
 	
 	
 }
--- e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
@@ -1,10 +1,17 @@
 package cn.e3mall.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
+import cn.e3mall.common.pojo.DataGridResult;
 import cn.e3mall.mapper.TbItemMapper;
 import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemExample;
 import cn.e3mall.service.ItemService;
 @Service
 public class ItemServiceImpl implements ItemService{
@@ -17,6 +24,19 @@ public class ItemServiceImpl implements ItemService{
 		TbItem selectByPrimaryKey = tbItemMapper.selectByPrimaryKey(id);
 		 
 		return selectByPrimaryKey;
+	}
+
+	@Override
+	public DataGridResult findAllItem(int page, int rows) {
+		PageHelper.startPage(page, rows);
+		DataGridResult dataGrIdResult = new DataGridResult();
+		TbItemExample example = new TbItemExample();
+		List<TbItem> list = tbItemMapper.selectByExample(example);
+		PageInfo<TbItem> info = new PageInfo<>(list);
+		long total = info.getTotal();
+		dataGrIdResult.setRows(list);
+		dataGrIdResult.setTotal(total);
+		return dataGrIdResult;
 	}
 
 }
